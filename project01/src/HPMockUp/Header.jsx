02a28272258c932606305d448b6c07877ad28a10
momoeigeta <momoe.igeta@force-corp.co.jp>
2021-07-14 12:09:03 +0900

    追加

---
+++ project01/src/HPMockUp/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles/Contact.css';
+
+const Header = () => {
+
+    return (
+        <header className="header" >
+            <div className="back">
+
+                <h1>お問い合わせ</h1>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
---
+++ project01/src/HPMockUp/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from './Header';
+import Button from './Button';
+import './styles/Contact.css';
+import Footer from './Footer';
+
+const Home = () => {
+
+    return (
+        <div>
+            <body className="body">
+
+                <div>
+                    <Header />
+                    < Button />
+                </div>
+
+                <Footer />
+            </body>
+        </div >
+    );
+};
+export default Home;
