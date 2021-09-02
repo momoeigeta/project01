a0084f1f03a61a50ffe5699e10ef1f7762ed3625
momoeigeta <momoe.igeta@force-corp.co.jp>
2021-09-02 16:59:05 +0900

    変更

--- project01/src/HPMockUp/Footer.jsx
+++ project01/src/HPMockUp/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 const Footer = () => {
 
     return (
-        <div>
+        <div className="Footer">
             <p className="c">お電話にてお問い合わせ頂く場合、フォース株式会社【総合係】まで
             </p>
             <div className="c"><span className="red">TEL 03-5259-5231</span>
